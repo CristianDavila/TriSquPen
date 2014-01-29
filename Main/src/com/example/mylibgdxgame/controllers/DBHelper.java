92d75c619b49967dff5a2ad58672f893af753680
Cristian <xp4@hotmail.com>
2014-01-29 22:07:57 +0100

    SQLite

--- Main/src/com/example/mylibgdxgame/controllers/DBHelper.java
+++ Main/src/com/example/mylibgdxgame/controllers/DBHelper.java
@@ -1,43 +1,16 @@
 package com.example.mylibgdxgame.controllers;
 
-import com.example.mylibgdxgame.interfaces.ActionResolver;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.badlogic.gdx.Game;
 
 
+public class DBHelper extends Game/ApplicationListener {
+        public ActionResolver mNativeFunctions;
 
-import android.content.Context;
-import android.os.Handler;
-import android.util.Log;
-import android.widget.Toast;
-
-public class DBHelper implements ActionResolver {
-
-    Handler uiThread;
-    Context appContext;
-
-    public DBHelper(Context appContext) {
-        uiThread = new Handler();
-        this.appContext = appContext;
-    }
-
-    @Override
-    public Connection getConnection() {
-        String url = "jdbc:sqldroid:/data/data/my.app.name/databases/data.sqlite";
-        try {
-            Class.forName("org.sqldroid.SQLDroidDriver").newInstance();
-            return DriverManager.getConnection(url);
-        } catch (InstantiationException e) {
-            Log.e("sql", e.getMessage());
-        } catch (IllegalAccessException e) {
-            Log.e("sql", e.getMessage());
-        } catch (ClassNotFoundException e) {
-            Log.e("sql", e.getMessage());
-        } catch (SQLException e) {
-            Log.e("sql", e.getMessage());
+        public MyGame(ActionResolver nativeFunctions) {
+            mNativeFunctions = nativeFunctions;
         }
-        return null;
-    }
+        // Exemplary function call, of course this doesn't make sense in render() ;)
+        public void render() {
+            mNativeFunctions.openURL("http://www.example.com");
+        }
 }
--- Main/src/com/example/mylibgdxgame/interfaces/ActionResolver.java
+++ Main/src/com/example/mylibgdxgame/interfaces/ActionResolver.java
@@ -1,11 +1,11 @@
 package com.example.mylibgdxgame.interfaces;
 
-import java.sql.Connection;
-
 /**
  * Created by salamandra on 28/01/14.
  */
 
 public interface ActionResolver {
-    public Connection getConnection();
+
+    public void openURL(String url);
+
 }
